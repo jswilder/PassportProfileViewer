3270243a21e704424276a9fc364f050c66a2d437
Jeremy W <jeremy.wilder@in-tech.com>
2019-02-15 09:24:38 -0500

    Save

--- app/src/main/java/com/jwilder/passportprofileviewer/viewmodels/ProfilesListViewModel.kt
+++ app/src/main/java/com/jwilder/passportprofileviewer/viewmodels/ProfilesListViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import com.google.firebase.firestore.FirebaseFirestore
 import com.jwilder.passportprofileviewer.classes.Profile
 import java.lang.Exception
+import java.util.ArrayList
 
 class ProfilesListViewModel : ViewModel() {
 
@@ -13,20 +14,41 @@ class ProfilesListViewModel : ViewModel() {
     private val mFireStore: FirebaseFirestore = FirebaseFirestore.getInstance() // The one we wanted...
     private var profiles: MutableLiveData<List<Profile>> = MutableLiveData()
 
+    /*
+    Needed Indexes:
+        GENDER
+            - Age ASC
+            - Age DESC
+            - Name ASC
+            - Name DESC
+        All
+            - Age ASC
+            - Age DESC
+            - Name ASC
+            - Name DESC
+     */
+
     init {
         // Load the initial profiles
+        val _profiles: MutableList<Profile> = ArrayList()
+
         val query = mFireStore.collection("Profiles")
+//            .whereEqualTo("age",26)
+            .whereEqualTo("gender","male")
+            .orderBy("uid")
             .get()
             .addOnSuccessListener { result ->
                 for(document in result) {
                     try {
                         // TODO Convert to live data
-                        val item: Profile = Profile(document)
+                        val item = Profile(document)
+                        _profiles.add(item)
                         Log.d(TAG,"$item")
                     } catch (e: Exception) {
                         Log.w(TAG,"Failed to convert.",e)
                     }
                 }
+                profiles.value = _profiles
             }
             .addOnFailureListener{ exception ->
                 Log.w(TAG,"Error getting docs.",exception)
