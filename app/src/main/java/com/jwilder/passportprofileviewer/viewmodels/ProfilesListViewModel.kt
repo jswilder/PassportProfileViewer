985b443ce66fe89448448697d6804ab1f842a6b4
Jeremy W <jeremy.wilder@in-tech.com>
2019-02-15 07:49:19 -0500

    FireStore: Have a basic initial query to retrieve the dummy data

--- app/src/main/java/com/jwilder/passportprofileviewer/viewmodels/ProfilesListViewModel.kt
+++ app/src/main/java/com/jwilder/passportprofileviewer/viewmodels/ProfilesListViewModel.kt
@@ -1,36 +1,30 @@
 package com.jwilder.passportprofileviewer.viewmodels
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
 import com.jwilder.passportprofileviewer.classes.Profile
 
 class ProfilesListViewModel : ViewModel() {
 
-    private val mDatabase: DatabaseReference = FirebaseDatabase.getInstance().getReference("Profiles")
-    var profiles: MutableLiveData<List<Profile>> = MutableLiveData()
+    private val TAG = "PROFILES_VIEW_MODEL"
+//    private val mDatabaseReference: DatabaseReference = FirebaseDatabase.getInstance().getReference("profiles") // Database
+    private val mFireStore: FirebaseFirestore = FirebaseFirestore.getInstance() // The one we wanted...
+    private var profiles: MutableLiveData<List<Profile>> = MutableLiveData()
 
-//    fun getProfiles() : LiveData<List<Profile>> {
-//
-//    }
+    init {
+        Log.w(TAG,"Ran init")
 
-    /*
-      class ArticleViewModel : ViewModel() {
-      var articles: MutableLiveData<List<Article>> = MutableLiveData()
-
-      fun getArticles(): LiveData<List<Article>> {
-        if (articles.value == null) {
-          FirebaseDatabase.getInstance()
-           .getReference("feed/articles")
-           .addListenerForSingleValueEvent(object : ValueEventListener {
-               override fun onDataChange(dataSnapshot: DataSnapshot) {
-                   if (dataSnapshot.exists()) {
-                       articles.postValue(toArticles(snapshot))
-                   })
-        }
-
-        return articles
+        val query = mFireStore.collection("Profiles")
+            .get()
+            .addOnSuccessListener { result ->
+                for(document in result) {
+                    Log.d(TAG,"${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener{ exception ->
+                Log.w(TAG,"Error getting docs.",exception)
+            }
     }
-     */
 }
