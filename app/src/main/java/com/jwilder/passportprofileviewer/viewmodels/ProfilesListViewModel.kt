dd456969b75b4929dc1f593826430ada004bba4e
Jeremy <jeremy.w.wilder@gmail.com>
2019-02-19 17:37:18 -0500

    Save: Simply moved some code into a method

--- app/src/main/java/com/jwilder/passportprofileviewer/viewmodels/ProfilesListViewModel.kt
+++ app/src/main/java/com/jwilder/passportprofileviewer/viewmodels/ProfilesListViewModel.kt
@@ -52,7 +52,10 @@ class ProfilesListViewModel : ViewModel() {
 
     init {
         loadInitialValues()
+        setLiveUpdateListener()
+    }
 
+    private fun setLiveUpdateListener() {
         mFireStore.collection(COLLECTION)
             .addSnapshotListener { snapshot, e ->
                 if( e != null ) {
